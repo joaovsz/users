a60748eb44cb73155f7a6a3f92bed84356bd1f70
joaovsz <joaovitor1713coin@gmail.com>
2023-07-24 08:13:31 -0300

    teste

--- src/pages/Cadastrar/Cadastro.tsx
+++ src/pages/Cadastrar/Cadastro.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Header } from '../Components/Header/Header'
 
-const Cadastro = () => {
+export const Cadastro = () => {
   return (
+    <>
     <Header/>
+    </>
   )
 }
-
-export default Cadastro
